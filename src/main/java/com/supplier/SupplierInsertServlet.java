1c77f856addb1251d140df3fd076a01593b0fabe
GRieZzLEO <chamodhleo1@gmail.com>
2022-05-10 00:28:03 +0530

    Last Push for the Sem

--- src/main/java/com/supplier/SupplierInsertServlet.java
+++ src/main/java/com/supplier/SupplierInsertServlet.java
@@ -1,11 +1,15 @@
 package com.supplier;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.customer.CustomerDBUtil;
 
 /**
  * Servlet implementation class SupplierInsertServlet
@@ -35,7 +39,25 @@ public class SupplierInsertServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String company=request.getParameter("company");
+		String branch=request.getParameter("branch");
+		String mobile=request.getParameter("mobile");
+		String uname=request.getParameter("uname");
+		String pwd=request.getParameter("pwd");
+		
+		boolean isTrue;
+		
+		isTrue = SupplierDBUtil.insertSupplier(company,branch,mobile,uname,pwd);
+		
+		if(isTrue==true) {
+			RequestDispatcher dis = request.getRequestDispatcher("home.jsp");
+			dis.forward(request, response);
+		}else {
+			RequestDispatcher dis2 = request.getRequestDispatcher("home.jsp");
+			dis2.forward(request, response);
+		}
 	}
 
 }
