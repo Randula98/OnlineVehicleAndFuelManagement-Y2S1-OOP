35c433ae975a1985c2a4bc2ab17fcbfd14a43c22
SuDelk <sudul.fernando@gmail.com>
2022-05-09 17:06:02 +0530

    additem

---
+++ src/main/java/com/item/AddItemServlet.java
@@ -0,0 +1,61 @@
+package com.item;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.customer.CustomerDBUtil;
+
+/**
+ * Servlet implementation class AddItemServlet
+ */
+@WebServlet("/AddItemServlet")
+public class AddItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String item=request.getParameter("item");
+		double price=request.getParameter("price");
+		int quantity=request.getParameter("quantity");
+		
+		
+		boolean isTrue;
+		
+		isTrue = ItemDBUtil.insertItem(item,price,quantity,supID);
+		
+		if(isTrue==true) {
+			RequestDispatcher dis = request.getRequestDispatcher("home.jsp");
+			dis.forward(request, response);
+		}else {
+			RequestDispatcher dis2 = request.getRequestDispatcher("home.jsp");
+			dis2.forward(request, response);
+		}
+	}
+
+}
