c778ba37b41713be9a93abfecf9c3820cb66d17e
GRieZzLEO <chamodhleo1@gmail.com>
2022-05-09 13:53:09 +0530

    fuck this shit

--- src/main/java/com/appointment/AppointmentDBUtil.java
+++ src/main/java/com/appointment/AppointmentDBUtil.java
@@ -1,7 +1,7 @@
 package com.appointment;
 
 import java.sql.Connection;
-
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import service.util.DBConnect;
@@ -9,8 +9,9 @@ import service.util.DBConnect;
 public class AppointmentDBUtil {
 	
 	private static boolean isSuccess;
-	private static Connection con = null;
+	private static Connection conn = null;
 	private static Statement state = null;
+	private static ResultSet rs = null;
 	
 	
 	public static boolean deleteAppointment(String id) {
@@ -30,6 +31,26 @@ public class AppointmentDBUtil {
 			}else {
 				isSuccess=false;
 			}
+		}catch(Exception X) {
+			X.printStackTrace();
+		}
+		return isSuccess;
+	}
+	
+	public static boolean addappointment(String id,String serve,String date) {
+		
+		boolean isSuccess = false;
+		
+		try {
+				conn = DBConnect.getConnection();
+				state = conn.createStatement();
+				String sql = "insert into appointment values(0,'"+serve+"','"+date+"','"+id+"')";
+				int rs = state.executeUpdate(sql);
+				if(rs>0) {
+					isSuccess=true;
+				}else {
+					isSuccess=false;
+				}
 		}catch(Exception X) {
 			X.printStackTrace();
 		}
